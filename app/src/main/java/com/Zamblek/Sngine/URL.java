1e938da19868a4c8c2060f5f0ed123f063c01650
dontka <105787916+dontka@users.noreply.github.com>
2024-04-27 02:22:49 +0200

    Update URL.java

--- app/src/main/java/com/Zamblek/Sngine/URL.java
+++ app/src/main/java/com/Zamblek/Sngine/URL.java
@@ -2,5 +2,5 @@ package com.dontka.Sngine;
 
 public class URL {
 
-    public static final  String url = "https://fylancer.blogspot.com/siteclient" ;
+    public static final  String url = "http://fylancer.000webhostapp.com/siteclient/" ;
 }
